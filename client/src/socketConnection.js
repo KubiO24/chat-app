bdb71eeea78a9d7a5dfe4a5a2d80e4cfb7359009
Kuba <kubakowal2004@gmail.com>
2022-07-09 18:02:36 +0200

    Heroku commit

--- client/src/socketConnection.js
+++ client/src/socketConnection.js
@@ -1,5 +1,8 @@
 import io from 'socket.io-client'
 
 export let socket;
-
-socket = io('/');
+// if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+//     socket = io.connect('http://localhost:3001')
+// } else {
+    socket = io('/');
+// }
