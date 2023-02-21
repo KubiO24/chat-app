628561d0f38a91d743049aeaee27ea0d73bee53d
KubiO24 <kubakowal2004@gmail.com>
2023-02-21 20:31:35 +0100

    Update package.json

--- client/src/socketConnection.js
+++ client/src/socketConnection.js
@@ -1,8 +1,8 @@
-import io from 'socket.io-client'
+import io from "socket.io-client";
 
 export let socket;
-if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
-   socket = io.connect('http://localhost:3001')
+if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
+    socket = io.connect("http://localhost:3001");
 } else {
-   socket = io('/');
+    socket = io("/");
 }
